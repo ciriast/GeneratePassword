8de92ea6a43bdc35381e2fd22a27ae486d686dd0
Carlos Irias <tcaitirias@outlook.com>
2023-08-21 05:46:37 -0600

    feat: Adds CharPool class and a static property called ValidChars.

---
+++ GeneratePassword/CharPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePassword
+{
+    internal class CharPool
+    {
+        public static string ValidChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+[]{}|;:,.<>?";
+    }
+}
